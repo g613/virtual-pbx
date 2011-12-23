fce87db3e44e2459b989516249dedb939b1165c2
igor.okunev@gmail.com <igor.okunev@gmail.com@f5450364-bfea-11de-93d8-795327aa6b9b>
2011-12-23 13:42:03 +0000

    misprint

--- contrib/xvb.sql
+++ contrib/xvb.sql
@@ -401,7 +401,7 @@ create table VPBX_DIDS_ATTR
 
     unique			    (DID),
     CONSTRAINT FK_VPBX_DIDS_ATTR_LANG FOREIGN KEY (LANG_ID) REFERENCES VPBX_LANG(ID) ON UPDATE CASCADE,
-    CONSTRAINT PK_VPBX_DIDS_ATTR PRIMARY KEY (ID)
+    CONSTRAINT PK_VPBX_DIDS_ATTR PRIMARY KEY (DID)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 create table VPBX_DIDS
@@ -2260,7 +2260,7 @@ insert into VPBX_SIPPEERS_TEMPLATES(HOST,NAME,DATA) VALUES('sip.telphin.com','Te
 insert into VPBX_SIPPEERS_TEMPLATES(HOST,NAME,DATA) VALUES('sipnet.ru','Sipnet',"$_[0]->{'fromdomain'}='sipnet.ru'; $_[0]->{'videosupport'}='no'; $_[0]->{'fromuser'}=$_[0]->{'defaultuser'}=$_[0]->{'username'}; $_[0]->{'dtmfmode'}='rfc2833'; $_[0]->{'disallow'}='all'; $_[0]->{'allow'}='alaw,ulaw'; $_[0]->{'port'}='5060';");
 insert into VPBX_SIPPEERS_TEMPLATES(HOST,NAME,DATA) VALUES('skype.sipnet.ru','Sipnet-Skype',"$_[0]->{'fromdomain'}='sipnet.ru'; $_[0]->{'videosupport'}='no'; $_[0]->{'fromuser'}=$_[0]->{'defaultuser'}=$_[0]->{'username'}; $_[0]->{'dtmfmode'}='rfc2833'; $_[0]->{'disallow'}='all'; $_[0]->{'allow'}='alaw,ulaw'; $_[0]->{'port'}='5060';");
 
-insert insert VPBX_DIDS_ATTR(DID,LANG_ID) VALUES( '613', 0 );
+insert into VPBX_DIDS_ATTR(DID,LANG_ID) VALUES( '613', 0 );
 
 -- OPENSER REGISTRAR
 create or replace view subscriber  ( id, username, domain, password, email_address, ha1, ha1b, rpid )
