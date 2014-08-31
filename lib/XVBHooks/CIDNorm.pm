90af01d5d0a061dc40415e73bdeb3d7b8fa8fe81
igor.okunev@gmail.com <igor.okunev@gmail.com@f5450364-bfea-11de-93d8-795327aa6b9b>
2014-08-31 19:18:16 +0000

    sync with 8378

--- lib/XVBHooks/CIDNorm.pm
+++ lib/XVBHooks/CIDNorm.pm
@@ -1,6 +1,6 @@
 ########################################################################
 #
-# $Id: CIDNorm.pm,v 1.2 2014/07/19 18:30:19 gosha Exp $
+# $Id: CIDNorm.pm,v 1.3 2014/08/28 11:15:26 gosha Exp $
 #
 #	Copyright (c) Igor Okunev <igor[at]prv.mts-nn.ru>
 #
@@ -25,7 +25,7 @@ sub call_start {
 	my $obj = shift;
 
 	if ( $obj->{'_CDR'}->{'CALL_TYPE'} ne 'transit' and $obj->{'_CDR'}->{'CALL_TYPE'} ne 'internal' ) {
-		$obj->{'_AGI'}->set_variable( 'CALLERID(num)', $obj->{'_CDR'}->{'CALLER_ID'} );
+		$obj->{'_AGI'}->set_variable( 'CALLERID(num)', (split(/\@/,$obj->{'_CDR'}->{'CALLER_ID'}))[0] );
 	}
 }
 
